826f2da393e8ecf1a4b77105e65ffe397eefe7df
lpitto86 <luca.p481@gmail.com>
2024-01-11 16:25:04 +0100

    Milestone2

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,13 @@ new Vue({
                   { text: 'Fare gli esercizi', done: false },
                   // Aggiungi altri "to do" secondo necessità
             ]
+      },
+      methods: {
+            removeTodo: function(todo) {
+                  const index = this.todos.indexOf(todo);
+                  if (index !== -1) {
+                  this.todos.splice(index, 1);
+                  }
+            }
       }
 });
