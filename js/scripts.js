e911e6862fdc3e83a0603cdac2045f8cd891dbfb
lpitto86 <luca.p481@gmail.com>
2024-01-11 16:16:12 +0100

    Milestone1

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+new Vue({
+      el: '#app',
+      data: {
+            todos: [
+                  { text: 'Fare la spesa', done: false },
+                  { text: 'Pulire la casa', done: true },
+                  { text: 'Fare gli esercizi', done: false },
+                  // Aggiungi altri "to do" secondo necessità
+            ]
+      }
+});
